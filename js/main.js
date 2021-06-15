08c7cc7dd244a2db496f31765844a872b5eede17
Sergey <sergeitruhan@gmail.com>
2021-06-15 13:04:32 +0300

    Добавляем функцию getRandomIntInclusive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 function getRandomArbitrary(min, max, maxDigits = 0) {
-    if (min > max || min < 0 || max <= 0) {
-      return ('Задан неверный диапазон! Укажите другие числа.');
-    }
-
-    const digitsDegree = 10 ** maxDigits;
-    return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+  if (min > max || min < 0 || max <= 0) {
+    return ('Задан неверный диапазон! Укажите другие числа.');
   }
-  getRandomArbitrary(1, 2, 6);
+  const digitsDegree = 10 ** maxDigits;
+  return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+}
+getRandomArbitrary(1, 2, 6);
+function getRandomIntInclusive(min, max) {
+  if (min >= max || min <= 0 || max <= 0) {
+    return ('Выбран неверный диапазон');
+  } else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+getRandomIntInclusive (6, 4);
+/*const getRandomArrayElement = (elements) => {
+  return elements[_.random (0, elements.length - 1)];
+};
+getRandomArrayElement();
+*/
